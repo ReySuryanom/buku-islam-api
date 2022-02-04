e9e38b75f622b7dc118f24966671bb7bb9a7d7d8
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-04 17:59:50 +0700

    create books controllers

---
+++ controllers/books.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+export const getBooks = (req, res) => {
+  const searchQuery = req.query.search;
+  fs.readFile(
+    new URL('../books/al-quran-dan-tafsir.json', import.meta.url),
+    'utf8',
+    (err, data) => {
+      const users = JSON.parse(data);
+      // console.log(
+      //   users.data.filter(({ firstname }) => firstname === searchQuery).length
+      // );
+      //   const x = data.
+      console.log(data);
+      res.end(data);
+    }
+  );
+};
+
+export const getBookDetail = (req, res) => {
+  fs.readFile(
+    new URL('../books/al-quran-dan-tafsir.json.json', import.meta.url),
+    'utf8',
+    (err, data) => {
+      const users = JSON.parse(data);
+      const user = users[`user${req.params.id}`];
+      res.end(JSON.stringify(user));
+    }
+  );
+};
