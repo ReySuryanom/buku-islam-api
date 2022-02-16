73b2d1ed64b5adb28ec5e31404b7426024e604ac
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-02-16 21:54:47 +0700

    update books controller

--- controllers/books.js
+++ controllers/books.js
@@ -1,25 +1,82 @@
 import fs from 'fs';
 
-export const getBooks = (req, res) => {
-  const searchQuery = req.query?.search;
-  const bookId = req.query?.bookId;
+const FILEPATHS = [
+  new URL('../books/akhlak.json', import.meta.url),
+  new URL('../books/al-quran-dan-tafsir.json', import.meta.url),
+  new URL('../books/aqidah.json', import.meta.url),
+  new URL('../books/fiqih-ibadah.json', import.meta.url),
+  new URL('../books/fiqih-muamalat.json', import.meta.url),
+  new URL('../books/fiqih-wanita.json', import.meta.url),
+  new URL('../books/hadits.json', import.meta.url),
+  new URL('../books/kajian-tematik.json', import.meta.url),
+  new URL('../books/sirah-dan-biografi.json', import.meta.url),
+  new URL('../books/ushul-fiqih.json', import.meta.url),
+];
 
-  fs.readFile(
-    new URL('../books/aqidah.json', import.meta.url),
-    'utf8',
-    (err, data) => {
-      let response = JSON.parse(data);
-      if (bookId) {
-        response = response.find((item) => item.id === bookId);
-      }
-      if (searchQuery) {
-        response = response.content.filter(({ text }) =>
-          text.includes(searchQuery)
-        );
-      }
-      res.end(JSON.stringify(response));
-    }
-  );
+export const getBooks = (req, res) => {
+  const bookId = req.query?.bookId;
+  const searchQuery = req.query?.search;
+
+  const promises = FILEPATHS.map((_path) => {
+    return new Promise(
+      function (_path, resolve, reject) {
+        fs.readFile(_path, 'utf8', (err, data) => {
+          if (err) {
+            resolve('');
+          } else {
+            resolve(data);
+          }
+        });
+      }.bind(this, _path)
+    );
+  });
+
+  if (bookId) {
+    let response = null;
+    Promise.all(promises).then((results) => {
+      results.forEach((content) => {
+        const books = JSON.parse(content);
+        // response = books.find((data) => data.id === bookId);
+        response = content;
+      });
+    });
+    console.log(response);
+    res.end(JSON.stringify(response));
+    // fs.readFile(
+    //   new URL(`../books/al-quran-dan-tafsir.json`, import.meta.url),
+    //   'utf8',
+    //   (err, data) => {
+    // let response = JSON.parse(data);
+    // if (bookId) {
+    //   response = response.find((item) => item.id === bookId);
+    // }
+    // if (searchQuery) {
+    //   response = response.content.filter(({ text }) =>
+    //     text.includes(searchQuery)
+    //   );
+    // }
+    // res.end(JSON.stringify(response));
+    //   }
+    // );
+  } else {
+    Promise.all(promises).then((results) => {
+      let bookId = [];
+      results.forEach((content) => {
+        const books = JSON.parse(content);
+        books.forEach(({ id, info }) => {
+          const category = info?.category || 'test';
+          bookId.push({
+            id,
+            category: category
+              .toLowerCase()
+              .replaceAll(' ', '-')
+              .replace('&', 'dan'),
+          });
+        });
+      });
+      res.end(JSON.stringify(bookId));
+    });
+  }
 };
 
 export const getCategories = (req, res) => {
@@ -43,6 +100,10 @@ export const getCategoryBooks = (req, res) => {
     }
   );
 };
+
+// if (searchQuery) {
+//   response = response.content.filter(({ text }) => text.includes(searchQuery));
+// }
 
 // export const getSpecificContent = (req, res) => {
 //   fs.readFile(
